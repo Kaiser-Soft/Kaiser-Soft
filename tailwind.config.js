b375c5513a62639afa9fdca5e8ad6ec8f8016e3d
Leonardo Ramirez <juanlov4321@gmail.com>
2024-05-13 23:09:51 -0500

    chore: Update tailwind.config.js with custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
 	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
 	plugins: [require("daisyui")],
 	daisyui: {
-		themes: ["light", "dark"],
+		themes: [
+			{
+				mytheme: {
+					primary: "#9B5DE5", // Un tono morado como el principal del logo
+					secondary: "#F15BB5", // Un tono rosado como un color secundario en el logo
+					accent: "#00BBF9", // Un azul claro como el color de acento visible en el logo
+					neutral: "#070D0D", // Un negro o gris muy oscuro para los neutros
+					"base-100": "#FFFFFF", // Blanco para el fondo base, podría ajustarse si es necesario
+					info: "#00F5D4", // Un cian brillante para información
+					success: "#8AC926", // Un verde vivo para éxitos
+					warning: "#FFCA3A", // Amarillo para advertencias
+					error: "#EF476F", // Rojo para errores
+				},
+			},
+		],
 	},
 };
